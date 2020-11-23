e7364aecb5fcdb1db9dfb1b8c998771f3c772c19
xuxinyi2019302190076 <1435223639@qq.com>
2020-11-23 16:22:51 +0800

    第十二周提交

--- yunpan/news/admin.py
+++ yunpan/news/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Article)
-admin.site.register(models.Reporter)
+#admin.site.register(models.Article)
+#admin.site.register(models.Reporter)
+admin.site.register(models.Student)
+#admin.site.register(models.Homework)
--- yunpan/news/views.py
+++ yunpan/news/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+'''
 from .models import Article
 
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {'year': year, 'article_list': a_list}
     return render(request, 'news/year_archive.html', context)
+'''    
+from .models import Student, Homework
+
+from django.views.generic.edit import CreateView
+
+class HomeworkCreate(CreateView):
+    model = Homework
+    template_name = 'homework_form.html'
+    fields = ['headline','attach','remark', 'student']
+
+
